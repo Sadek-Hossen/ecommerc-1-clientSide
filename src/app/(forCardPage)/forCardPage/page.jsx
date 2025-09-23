70ea7d69527a29ccd0113a858f47f60457fd6840
Sadek-Hossen <hossensadek726@gmail.com>
2025-09-23 22:15:29 +0600

    build done

--- src/app/(forCardPage)/forCardPage/page.jsx
+++ src/app/(forCardPage)/forCardPage/page.jsx
@@ -4,7 +4,6 @@ import React from 'react'
 function ForCardPage() {
   return (
     <div>
-        
         <CardPage />
     </div>
   )
--- src/app/(payment)/payment/page.jsx
+++ src/app/(payment)/payment/page.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import Payment from '../../compunents/layouts/payment/payment'
 
 function PaymentPage() {
@@ -6,7 +6,9 @@ function PaymentPage() {
     <div>
    <div className='max-w-4xl mx-auto py-10 shadow-blue-600 rounded-2xl mt-3 bg-blue-200'>
          <h1 className='text-2xl font-bold font-mono text-center'>Your Payment </h1>
-         <Payment />
+      <Suspense fallback={<div>Loading...</div>}>
+      <Payment />
+    </Suspense>
         
    </div>
     </div>
